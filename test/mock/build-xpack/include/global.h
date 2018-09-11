086b3ef922ebef022925930413214e988ee80df5
Liviu Ionescu <ilg@livius.net>
2018-09-11 20:31:23 +0300

    add 'includeFiles' & 'includeSystemFolders'

---
+++ test/mock/build-xpack/include/global.h
@@ -0,0 +1,6 @@
+#ifndef GLOBAL_H_
+#define GLOBAL_H_
+
+#define GLOBAL1
+
+#endif /* GLOBAL_H_ */
---
+++ test/mock/build-xpack/include/global2.h
@@ -0,0 +1,6 @@
+#ifndef GLOBAL2_H_
+#define GLOBAL2_H_
+
+#define GLOBAL2
+
+#endif /* GLOBAL2_H_ */
